fa4cf7241ff321df8b47a9d440e76349d4dbcaee
WalkWeb <av18775@list.ru>
2024-12-29 00:10:43 +0300

    Added ArmorType

--- src/Item/ItemException.php
+++ src/Item/ItemException.php
@@ -12,5 +12,6 @@ class ItemException extends Exception
     public const UNKNOWN_SECTION_TYPE = 'Unknown section type';
     public const UNKNOWN_EQUIP_TYPE   = 'Unknown equip type';
     public const UNKNOWN_WEAPON_TYPE  = 'Unknown weapon type';
+    public const UNKNOWN_ARMOR_TYPE   = 'Unknown armor type';
     public const UNKNOWN_POTION_TYPE  = 'Unknown weapon type';
 }
